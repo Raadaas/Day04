fa98d4bd6227180a6bd223b5a9bd42be851babb4
Raadaas <ivanradic515@gmail.com>
2021-03-12 00:16:26 +0100

    Added new files

---
+++ ex10/src/index.js
@@ -0,0 +1,14 @@
+function main () {
+    //Only change code below this line   
+    var myFirstName = "Ivan";
+    var myLastName = "Radic";
+    var myHomeTown = "Nevesinje";
+    //Only change code above this line   
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    }; 
+    }
+console.log(main());    
+module.exports = main;
